5b07cbd2ba27fa836ecc748f65ee6bee65f24cb9
haripopuri22 <100458927+haripopuri22@users.noreply.github.com>
2025-10-10 17:38:58 +0530

    project setup

---
+++ UserService/src/main/java/com/rohith/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rohith.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
